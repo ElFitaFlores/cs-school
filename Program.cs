44397bc2fe0e8ab64ae59b762f0785470cef58f0
Rafael Flores <fita500@gmail.com>
2021-01-12 19:22:33 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 1992);
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
