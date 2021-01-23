aeb39f4a5f61112caf15c849472396a6bd6be440
Rafael Flores <fita500@gmail.com>
2021-01-22 19:36:37 -0600

    add Curso entity

--- Program.cs
+++ Program.cs
@@ -9,8 +9,20 @@ namespace Etapa1
         {
             var escuela = new Escuela("Platzi Academy", 1992, TiposEscuela.Secundaria, pais: "Guatemala", ciudad: "Guate");
             escuela.TipoEscuela = TiposEscuela.Primaria;
+
+            var curso1 = new Curso()
+            {
+                Nombre = "101",
+            };
+
+            var curso2 = new Curso()
+            {
+                Nombre = "102",
+            };
             Console.WriteLine(escuela.Nombre);
             Console.WriteLine(escuela);
+            Console.WriteLine(curso1.UniqueId);
+            Console.WriteLine(curso2.UniqueId);
         }
     }
 }
