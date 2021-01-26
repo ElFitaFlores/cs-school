327cfa9761df742fab30a4ff99eb97d4eb6a0ebc
Rafael Flores <fita500@gmail.com>
2021-01-25 19:49:31 -0600

    Add array

--- Program.cs
+++ Program.cs
@@ -10,19 +10,32 @@ namespace Etapa1
             var escuela = new Escuela("Platzi Academy", 1992, TiposEscuela.Secundaria, pais: "Guatemala", ciudad: "Guate");
             escuela.TipoEscuela = TiposEscuela.Primaria;
 
-            var curso1 = new Curso()
+            var arregloCursos = new Curso[2];
+
+            arregloCursos[0] = new Curso()
             {
                 Nombre = "101",
             };
 
-            var curso2 = new Curso()
+            arregloCursos[1] = new Curso()
             {
                 Nombre = "102",
             };
             Console.WriteLine(escuela.Nombre);
             Console.WriteLine(escuela);
-            Console.WriteLine(curso1.UniqueId);
-            Console.WriteLine(curso2.UniqueId);
+            // Console.WriteLine(curso1.UniqueId);
+            // Console.WriteLine(curso2.UniqueId);
+            ImprimirCursos(arregloCursos);
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"El nombre del curso es {arregloCursos[contador].Nombre}");
+                contador += 1;
+            }
         }
     }
 }
